4f844b4975a2b9af7c6f342c17f998efa6855cb3
qianmo-su <3360147782@qq.com>
2026-05-19 10:59:50 +0800

    add RMSNorm

--- model/model.py
+++ model/model.py
@@ -70,3 +70,19 @@ class MokioMindConfig(PretrainedConfig):
             if self.inference_rope_scaling
             else None
         )
+        
+import torch
+import torch.nn as nn
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim:int, eps:float=1e-6):
+        super().__init__()
+        self.dim = dim
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        return torch.rsqrt(x.pow(2).mean(-1,keepdim=True)+self.eps)
+    
+    def forward(self, x):
+        return self.weight * self._norm(x.float()).type_as(x)
